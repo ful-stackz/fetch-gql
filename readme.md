6f146edc1b49de0a03ce40d114f172f20388cd28
Ivan Stoyanov <ivan.stoyanov@esightmonitoring.nl>
2021-04-24 18:40:42 +0200

    initial

---
+++ readme.md
@@ -0,0 +1,181 @@
+# Fetch GraphQL data
+
+A zero-dependency, typed GraphQL client library built on top of `fetch` that provides monadic-ish responses for the tiny extra but of safety.
+
+Same, but with bullets:
+
+- zero-dependencies, built entirely on top of `fetch`
+- execute GraphQL queries and mutations with type-safe variables and responses
+- handle responses explicitly in a monadic-ish way for the extra runtime safety
+
+## Install
+
+The package is hosted at [npm](https://npmjs.com/package/fetch-gql)
+
+```bash
+npm install --save fetch-gql
+```
+
+## Usage
+
+
+### Basic usage
+
+The most basic usage example is to create a GraphQL client instance, define some query/mutation types, execute them and handle the response.
+
+```typescript
+import createClient from 'fetch-gql'
+
+const client = createClient({
+  url: '<your-graphql-endpoint-url>'
+})
+
+type GetUserQuery = { user: { id: number; username: string } }
+type GetUserQueryVariables = { id: number }
+const getUserQuery = `
+  query getUser(id: Int!) {
+    user(id: $id) {
+      id
+      username
+    }
+  }
+`
+
+const response = await client.query<GetUserQuery, GetUserQueryVariables>({
+  query: getUserQuery,
+  variables: { id: 1 } // type checked agains GetUserQueryVariables
+})
+
+response.match({
+  ok: data => { // data is of type GetUserQuery
+    const { user } = data
+    console.log(user.id, user.username)
+  },
+  err: errors => { // errors is of type GraphQLError[]
+    // handle errors
+  }
+})
+```
+
+For details on the `.match()` and the rest of the response-handling API check out the [explicit response handling](#explicit-response-handling) point.
+
+### Custom error types
+
+The [GraphQL spec](http://spec.graphql.org/draft/#sec-Errors) defines a default error shape that looks like this in a JSON response:
+
+```json
+{
+  "error": [
+    {
+      "message": "Name for character with ID 1002 could not be fetched.",
+      "locations": [{ "line": 6, "column": 7 }],
+      "path": ["hero", "heroFriends", 1, "name"]
+    }
+  ]
+}
+```
+
+Accordingly, `fetch-gql` defines a default GraphQL error type which looks like this:
+
+```typesript
+type GraphQLError = {
+  message: string
+  locations: { line: number; column: number }[]
+  path: (string | number)[]
+}
+```
+
+But what if your server returns a custom error type? Whether your server follows the GraphQL error spec guidelines of utilising an `extensions` field which is a map of additional error details, or it returns a fully customized error type - `fetch-gql` has you covered!
+
+When creating a new GraphQL client you can provide the error type that you expect to be returned from the server. The client will then provide type-safety when handling errors!
+
+```typescript
+import createClient, { GraphQLError } from 'fetch-gql'
+
+type ServerErrorCode =
+  | 'auth/wrong-password'
+  | 'api/not-found'
+  | 'api/duplicate-key'
+
+// If the server follows the spec guidelines
+// simply extend the default error type with
+// a custom typed _extensions_ field
+type ExtendedGraphQLError = GraphQLError & {
+  extensions: {
+    code: ServerErrorCode
+  }
+}
+
+// If the server returns a fully customized error type
+// simply define the expected error type
+type CustomGraphQLError = {
+  message: string
+  code: ServerErrorCode
+}
+
+const guidelinesClient = createClient<ExtendedGraphQLError>({ /* client config */ })
+const response = await guidelinesClient.query<Query, QueryVariables>({ /* query config */ })
+response.err(errors => { }) // errors is of type ExtendedGraphQLError[]
+
+const customErrorClient = createClient<CustomGraphQLError>({ /* client config */ })
+const response = await customErrorClient.query({ /* query config */ })
+response.err(errors => { }) // errors is of type CustomGraphQLError[]
+```
+
+## Explicit response handling
+
+In order to provide an extra bit of safety, `fetch-gql` exposes a monadic-ish API for handling the response of the GraphQL query or mutation. If you are not acquinted with that style of programming don't worry it's really simple and easy to use.
+
+Each query/mutation executed via the client returns a response of type `GraphQLResponse<TData, TError>` where `TData` is the type of the data returned on a successful request and `TError` is the type of an error returned on a non-successful request. `fetch-gql` provides the following API for handling both cases:
+
+```typescript
+const response = await client.query({ /* query config */ })
+
+// get a boolean indicating whether the response contains data
+response.isOk
+
+// get a boolean indicating whether the response contains error(s)
+response.isErr
+
+// execute the provided function when the response contains data,
+// providing said data as an argument
+// if the response does not contain data the function is not executed
+response.ok(data => { })
+
+// execute the provided function when the response contains error(s),
+// providing said error(s) as an argument
+// if the response does not contain error(s) the function is not executed
+response.err(errors => { })
+
+// groups the .ok() and .err() in a single endpoint
+response.match({
+  ok: data => { },
+  err: errors => { }
+})
+
+// maps the response data, when available, to another shape _T_ and
+// returns a new GraphQLResponse<T, TError> object
+// if the response does not contain data the original is returned
+response.mapOk(data => { })
+// you can then chain one of the other functions described in this section
+response.mapOk(data => { })
+  .match({ ok: data => { }, err: errors => { } })
+
+// maps the response error(s), when available, to another shape _T_ and
+// returns a new GraphQLResponse<TData, T> object
+// if the response does not contain error(s) the original is returned
+response.mapErr(errors => { })
+// you can then chain any of the other functions described in this section
+response.mapErrors(errors => { })
+  .match({ ok: data => { }, err: errors => { } })
+```
+
+## Todo
+
+- [ ] link sample projects (git submodules?)
+- [ ] add tests
+- [ ] add nice documentation (with [docusaurus](https://docusaurus.io/)?)
+
+## Contribution
+
+Suggestions, insights, bug reports and code contributions are all welcome. File an issue or open a pull request - thank you!
---
+++ src/index.ts
@@ -0,0 +1,194 @@
+export type GraphQLError = {
+  message: string;
+}
+
+export type GraphQLResponse<TData, TError> = {
+  /**
+   * Indicates whether the response came back successful with data.
+   */
+  readonly isOk: boolean;
+
+  /**
+   * Executes the provided function if the response came back with data,
+   * providing the data as a parameter.
+   */
+  ok: (action: (data: TData) => void) => void;
+
+  /**
+   * Indicates whether the response came back unsuccessful with error(s).
+   */
+  readonly isErr: boolean;
+
+  /**
+   * Executes the provided function if the response came back with error(s),
+   * providing the error(s) as a parameter.
+   */
+  err: (action: (errors: TError[]) => void) => void;
+
+  /**
+   * Executes one of the provided functions depending on the status of the response.
+   * If it came back successful with data - the `ok` function is executed.
+   * Otherwise the `err` function is executed.
+   */
+  match: <T>(config: {
+    ok: (data: TData) => T;
+    err: (errors: TError[]) => T;
+  }) =>T;
+
+  /**
+   * Maps the data that came back with a successful response to another data shape `T` and
+   * returns a new `GraphQLResponse<T, TError>` object.
+   * If the response came back with errors the original object is returned,
+   * without applying the mapping function.
+   */
+  mapOk: <T>(mapFn: (data: TData) => T) => GraphQLResponse<T, TError>;
+
+  /**
+   * Maps the error(s) that came back with an unsuccessful response to another error shape `T` and
+   * returns a new `GraphQLResponse<TData, T>` object.
+   * If the response came back successful with data then the original object is returned,
+   * without applying the mapping function.
+   */
+  mapErr: <T>(mapFn: (errors: TError[]) => T) => GraphQLResponse<TData, T>;
+}
+
+export type GraphQLQueryConfig<TVariables> = {
+  query: string;
+  variables?: TVariables;
+}
+
+export type GraphQLClient<TError> = {
+  /**
+   * Execute a GraphQL query or mutation.
+   */
+  query: <TData = any, TVariables = never>(
+    config: GraphQLQueryConfig<TVariables>
+  ) => Promise<GraphQLResponse<TData, TError>>;
+}
+
+export type GraphQLClientConfig<TError> = {
+  /**
+   * The URL at which to send the GraphQL HTTP POST requests.
+   */
+  url: string;
+  /**
+   * Handle or map an HTTP error to the error type of the client.
+   * Return an empty array to "discard" errors.
+   * If this function is not provided the client will throw
+   * an Error when the HTTP request has a non-ok status.
+   */
+  onHttpError?: (request: Request, response: Response) => TError | TError[] | Promise<TError | TError[]>;
+  /**
+   * Handle or map GraphQL errors globally, before the query initiator
+   * gets back the response. Return an empty array to "discard" errors.
+   * If this function is not provided or returns a falsey value
+   * the original GraphQL errors are directly returned to the
+   * request initiator.
+   */
+  onError?: (errors: TError[]) => TError | TError[] | void | Promise<TError | TError[] | void>;
+  /**
+   * A collection of headers to be used in every single request.
+   */
+  headers?: Record<string, string>;
+  /**
+   * Modify the request before it is sent. This function will be invoked before
+   * each and every request is sent, providing the base request as a parameter.
+   * The base request will contain the basics - url, method, body, and all headers,
+   * if any, as provided with the `headers` config option.
+   * To modify the request do not mutate the parameter but return a new @see Request object to be used.
+   * The `createRequest` function creates a new Request object pre-populated with the basic details of the
+   * request - URL (from config option), method (POST), the standard content-type headers,
+   * body (from the request initiator; as stringified JSON), and all headers from the config option, if any.
+   */
+  preRequest?: (createRequest: () => Request) => Request | Promise<Request>;
+}
+
+function ok<TData>(data: TData): GraphQLResponse<TData, never> {
+  return {
+    isOk: true,
+    ok: action => action(data),
+    isErr: false,
+    err: () => {},
+    match: config => config.ok(data),
+    mapOk: mapFn => ok(mapFn(data)),
+    mapErr: () => ok(data),
+  };
+}
+
+function err<TError>(error: TError | TError[]): GraphQLResponse<never, TError> {
+  const errors = Array.isArray(error) ? error : [error];
+  return {
+    isOk: false,
+    ok: () => {},
+    isErr: true,
+    err: action => action(errors),
+    match: config => config.err(errors),
+    mapOk: () => err(errors),
+    mapErr: mapFn => err(mapFn(errors)),
+  };
+}
+
+async function query<TData, TVariables, TError>(
+  config: GraphQLQueryConfig<TVariables> & GraphQLClientConfig<TError>
+): Promise<GraphQLResponse<TData, TError>> {
+  const headers = new Headers();
+  headers.append('x-request-type', 'GraphQL');
+  headers.append('content-type', 'application/json');
+  headers.append('accept', 'application/json');
+
+  if (config.headers) {
+    Object.keys(config.headers).forEach(key => {
+      headers.append(key, config.headers[key]);
+    });
+  }
+
+  const body = JSON.stringify({
+    query: config.query,
+    variables: config.variables,
+  });
+
+  const baseRequest = new Request(config.url, { body, headers, method: 'POST' });
+  const request = config.preRequest
+    ? await Promise.resolve(config.preRequest(() => baseRequest.clone()))
+    : baseRequest;
+
+  const response = await fetch(request);
+
+  if (!response.ok) {
+    if (config.onHttpError) {
+      const mappedError = await Promise.resolve(config.onHttpError(request, response));
+      return err(mappedError);
+    }
+
+    throw new Error(`[GraphQL] HTTP request failed with status: ${response.status} (${response.statusText})`);
+  }
+
+  const { data, error } = await response.json();
+  if (data !== undefined) return ok<TData>(data);
+  if (error !== undefined) {
+    // if there is no data handler/mapper return the error right away
+    if (!config.onError) return err<TError>(error);
+
+    const errors = Array.isArray(error) ? error : [error];
+    const mappedError = await Promise.resolve(config.onError(error));
+    return err<TError>(mappedError ? mappedError : errors);
+  }
+
+  throw new Error(
+    '[GraphQL] Expected properties \'data\' or \'error\' in the response body. Neither was available.'
+  );
+}
+
+/**
+ * Creates a new GraphQL client with the provided configuration.
+ * @param config Configure the GraphQL client
+ */
+export default function createClient<TError = GraphQLError>(
+  config: GraphQLClientConfig<TError>
+): GraphQLClient<TError> {
+  return {
+    query: <TData, TVariables = never>(
+      queryConfig: GraphQLQueryConfig<TVariables>
+    ) => query<TData, TVariables, TError>({ ...config, ...queryConfig }),
+  }
+}
